bde5b9ebb73049653e2ebab8a014e94a612bece5
TuubaCa <tuubacakara@gmail.com>
2024-03-24 13:37:50 +0100

    'java son durum'

---
+++ src/j32_Enum/Runner.java
@@ -0,0 +1,19 @@
+package j32_Enum;
+
+public class Runner {
+    public static void main(String[] args) {
+    //Task-> verilen yilin ayini numarasina göre kac gun cektigini print eden code create ediniz.
+    Aylar ay=Aylar.MART; // Aylar enum data type ay variable tanimlanip
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
